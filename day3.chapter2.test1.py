e1782c160f56292a06b6a64b6fbcf64afeecfb0e
走水 <zhaotaoalex@outlook.com>
2021-10-11 03:10:30 +0800

    Delete day3.chapter2.test1.py

--- day3.chapter2.test1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pandas.io.parsers import read_csv
-
-df=read_csv('D:\下载\data\learn_pandas.csv',parse_dates=['Time_Record'])
-
-dfdemo=df[['Weight','Height']]
-s=pd.Series([1.235,2.788,2.0,6,7])
-print(s.clip(1,3).replace({3:100}))
